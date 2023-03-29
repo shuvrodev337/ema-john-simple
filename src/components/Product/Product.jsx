029cc52474d3efa0a9f8bbf08a1554e476342d6b
unknown <shuvrodevmondal337@gmail.com>
2023-03-29 19:27:56 +0600

    added event handler on add to cart button

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import './Product.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingBag } from '@fortawesome/free-solid-svg-icons'
+
 const Product = (props) => {
     const {name, seller, price, img, quantity, ratings}= props.product
+    const handleAddToCart=props.handleAddToCart
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -11,7 +15,10 @@ const Product = (props) => {
             <p>Manufacturer: {seller}</p>
             <p>Rating: {ratings}</p>
             </div>
-            <button className='btn-cart'>Add To cart</button>
+            <button onClick={()=>handleAddToCart(props.product)} className='btn-cart'>
+                Add To cart  &nbsp;
+                <FontAwesomeIcon icon={faShoppingBag} />
+            </button>
         </div>
     );
 };
